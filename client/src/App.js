4666e46c637899bc7530e1ed5470a558f65a7e2d
Wendy <yuanxie@outlook.com>
2019-08-08 11:43:04 -0400

    Set up the backend and frontend of the project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavBar from './components/AppNavBar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavBar/>
+    </div>
+  );
+}
+
+export default App;
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+    //Routing refers to how an application's endpoints(URIS) respond to client requests
+
 
 //Item Model
 const Item = require('../../models/Item')
@@ -9,9 +11,29 @@ const Item = require('../../models/Item')
 //@access  Public 
 router.get('/', (req, res) => {
     Item.find()
-        .sort({ date: -1})
+        .sort({ date: -1 })
         .then(items => res.json(items))
 })
 
 
- module.exports = router
+//@route  POST api/items
+//@desc   Create an item
+//@access  Public 
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    })
+
+    newItem.save().then(item => res.json(item))
+})
+
+//@route   DELETE api/items/:id
+//@desc    Detele an item
+//@access  Public 
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
+})
+
+module.exports = router
