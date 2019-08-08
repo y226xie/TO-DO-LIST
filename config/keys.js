7c8715aa8d62924e89cdb429c2d32eb9c4a1c01e
Yuan Xie <yuanxie@outlook.com>
2019-08-08 09:45:58 -0400

    Initial Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    monogoURI: 'mongodb+srv://wendy:M13950523520xy@cluster0-ujsku.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+//Item Model
+const Item = require('../../models/Item')
+
+//@route  GET api/items
+//@desc    GET All items
+//@access  Public 
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1})
+        .then(items => res.json(items))
+})
+
+
+ module.exports = router
